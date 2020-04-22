bdfd2e09db67a4043955ab79899f2266a10ded65
aculledor <aculledor@gmail.com>
2020-04-22 16:55:05 +0200

    Prueba de Git

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: acull
+ *
+ * Created on 22 de abril de 2020, 16:52
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("%d",1+1);
+    
+    return (EXIT_SUCCESS);
+}
+
